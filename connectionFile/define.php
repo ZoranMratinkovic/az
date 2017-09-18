978531133c8687b9e2adb40ed31b0f54a4409c40
Andrija Pavlovic <andrija.pavlovic996@gmail.com>
2017-09-18 11:55:34 +0200

    Push

--- connectionFile/define.php
+++ connectionFile/define.php
@@ -1,6 +1,6 @@
 <?php 
 
-	define("EMAIL","webtesting96@gmail.com");
+	define("EMAIL", "webtesting96@gmail.com");
 	define("NAME","Andrija Pavlovic");
 	define("USERNAME","webtesting96@gmail.com");
 	define("PASSWORD","web_testing");
