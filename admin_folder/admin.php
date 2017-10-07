2a21b68eede3232df5b11ad0eb013088d5bba667
zoki <zoran3001@gmail.com>
2017-10-07 15:59:35 +0200

    zoki

--- admin_folder/admin.php
+++ admin_folder/admin.php
@@ -72,7 +72,7 @@ if(isset($_SESSION['email'])&&$_SESSION['id_role']==1){ ?>
                     <a href="admin.php?word=news">newsletter</a>
                 </li>
                 <li>
-                    <a href="admin.php?word=answer">newsletter</a>
+                    <a href="admin.php?word=answer">Kommentare</a>
                 </li>
 
             </ul>
